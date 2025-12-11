bae41d61bceaff688daf03e8d4bc0a4da607ae59
rnhokak <81678171+rnhokak@users.noreply.github.com>
2025-12-11 15:44:14 +0700

    1

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -39,7 +39,7 @@ const CardContent = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("p-0 sm:p-3 sm:pt-0", className)} {...props} />
+  <div ref={ref} className={cn("p-1 sm:p-5 sm:pt-0", className)} {...props} />
 ));
 CardContent.displayName = "CardContent";
 
