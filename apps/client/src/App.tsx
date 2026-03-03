dc17e2af285f1137db24287d2573252c63c8b9fc
Friday <Fri@gamil.com>
2026-03-03 20:44:12 +0700

    11

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -2,12 +2,14 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
 import { QueryProvider } from './components/providers/QueryProvider'
 import { MoneyVisibilityProvider } from './components/providers/MoneyVisibilityProvider'
+import { Notifications } from './components/ui/notifications'
 
 function App() {
   return (
     <QueryProvider>
       <MoneyVisibilityProvider />
       <RouterProvider router={router} />
+      <Notifications />
     </QueryProvider>
   )
 }
